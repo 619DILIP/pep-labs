717413b3b5ee65a3a78bff7bdd85d6fea3174fde
Dilip Babu <dilip.babu@revature.com>
2023-01-12 10:53:37 +0000

    Saved progress on ReturnArrayContents 2023-01-12 10:53:37 +0000

--- ReturnArrayContents/src/main/java/ReturnArrayContents.java
+++ ReturnArrayContents/src/main/java/ReturnArrayContents.java
@@ -28,6 +28,14 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        StringBuilder myStringB = new StringBuilder();
+        String str = new String();
+    
+        for (int i : nums) {
+            myStringB.append(i+" ");
+        }
+    
+        str = myStringB.toString();
+        return str;
     }
 }
